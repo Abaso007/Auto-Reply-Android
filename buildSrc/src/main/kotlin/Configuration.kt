3fa8b5283669debd816144f601a31b42c47c9142
it5prasoon <prasoonk187@gmail.com>
2025-12-28 10:30:50 +0530

    feat: Update versioning scheme
    The version name format has been changed from a three-part (`major.minor.patch`) to a four-part (`major.year.minor.patch`) system. The version name is now "1.25.9.2".

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -3,9 +3,10 @@ object Configuration {
     const val targetSdk = 36
     const val minSdk = 23
     private const val majorVersion = 1
-    private const val minorVersion = 25
-    private const val patchVersion = 9.2
-    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+    private const val versionYear = 25
+    private const val minorVersion = 9
+    private const val patchVersion = 2
+    const val versionName = "$majorVersion.$versionYear.$minorVersion.$patchVersion"
     const val versionCode = 40
     const val artifactGroup = "com.matrix.autoreply"
 }
